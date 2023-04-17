f4cc6db13a22933e10be95a2a114313dd212bf42
rubeng07 <ruben.xaviergarcia93@gmail.com>
2023-04-17 23:30:22 +0100

    criação do Modelo (tabelas)

---
+++ CriadorDeCaes/CriadorDeCaes/Models/Fotografias.cs
@@ -0,0 +1,15 @@
+﻿namespace CriadorDeCaes.Models
+{
+    public class Fotografias
+    {
+        public int Id { get; set; }
+
+        public string Ficheiro { get; set; } 
+
+        public string Local { get; set;}
+
+        public DateTime Data { get; set; }
+
+
+    }
+}
