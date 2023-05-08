c737d48bfdd54ff78c88c9224573c7972d2b077a
rubeng07 <ruben.xaviergarcia93@gmail.com>
2023-05-08 23:18:54 +0100

    - Criação controller + Views para os 'Criadores' - Adição de anotadores ao modelo 'Criadores'

--- CriadorDeCaes/CriadorDeCaes/Models/Criadores.cs
+++ CriadorDeCaes/CriadorDeCaes/Models/Criadores.cs
@@ -1,4 +1,6 @@
-﻿namespace CriadorDeCaes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CriadorDeCaes.Models
 {
     /// <summary>
     /// Dados dos criadores dos cães
@@ -10,6 +12,11 @@
             ListaRacas = new HashSet<Racas>();
         }
 
+        /*
+         * Anotadores
+         * https://learn.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations
+         * https://www.entityframeworktutorial.net/code-first/dataannotation-in-code-first.aspx#google_vignette
+         */
 
         /// <summary>
         /// PK
@@ -19,31 +26,39 @@
         /// <summary>
         /// Nome do Criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string Nome { get; set; }
 
         /// <summary>
         /// Nome pelo qual o criador é conhecido no negócio de venda de cães
         /// </summary>
+        [Display(Name = "Nome Comercial")]
         public string NomeComercial { get; set;  }
 
         /// <summary>
         /// Morada do criador
         /// </summary>
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
         public string Morada { get; set; }
 
         /// <summary>
         /// Código Portal da morada do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [Display(Name = "Código Postal")]
         public string CodPostal { get; set; }
 
         /// <summary>
         /// Telemóvel do criador
         /// </summary>
+        [Display(Name = "Telemóvel")]
         public string Telemovel { get; set; }
 
         /// <summary>
         /// Email do criador
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [EmailAddress]
         public string Email { get; set; }
 
         // ************************************
